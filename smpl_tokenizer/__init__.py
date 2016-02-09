07a4f03aaffdfcda2144398642fb3102c1490eb8
Steven Cutting <steven.e.cutting@linux.com>
2016-02-08 23:54:14 -0600

    Added main functions to __init__.py

---
+++ smpl_tokenizer/__init__.py
@@ -0,0 +1,9 @@
+__title__ = 'smpl_tokenizer'
+__author__ = 'Steven Cutting'
+__author_email__ = 'steven.e.cutting@linux.com'
+__created_on__ = '02/06/2016'
+__copyright__ = "smpl_tokenizer Copyright (C) 2015  Steven Cutting"
+
+
+from smpl_tokenizer.tokenize import(uni_gram, bi_gram, tri_gram,
+                                    text_to_bow, text_to_uni_bow, text_to_bi_bow)
