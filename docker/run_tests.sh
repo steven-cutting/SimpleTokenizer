b69839cba39f10619fc3bf44ce7682de2c24a7c3
Steven Cutting <steven.e.cutting@linux.com>
2016-02-09 20:59:39 -0600

    integrated pytest with setuptools.

--- docker/run_tests.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-find . -name "test_*" -type d -exec sh -c '(cd {} && docker-compose run test)' ';'
--- docker/test_py3-4/docker_pytest.sh
+++ docker/test_py3-4/docker_pytest.sh
@@ -4,9 +4,7 @@ find . -name '*.pyc' -delete
 find . -name "*.egg-info" -type d -exec rm -r "{}" \;  2> /dev/null
 find . -name "*__pycache__" -type d -exec rm -r "{}" \;  2> /dev/null
 
-pip install -e .
-
-py.test --cache-clear
+python setup.py test
 
 find . -name '*.pyc' -delete
 find . -name "*.egg-info" -type d -exec rm -r "{}" \;  2> /dev/null
